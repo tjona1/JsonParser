77930080da9565fcf2b7606db44c438972c81f71
tjona <m. brambilla@presstoday.com>
2022-01-04 11:16:15 +0100

    Esempio di Json Parser creato una nupva classe CreateStaff

---
+++ JsonParser/src/main/java/com/example/JsonParser/CreateStaff.java
@@ -0,0 +1,29 @@
+package com.example.JsonParser;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CreateStaff {
+    public static Staff createStaff() {
+
+        Staff staff = new Staff();
+
+        staff.setName("tjona");
+        staff.setAge(29);
+        staff.setExperience(1);
+        staff.setPosition(new String[]{"Founder", "CTO", "Writer"});
+        Map<String, BigDecimal> salary = new HashMap() {{
+            put("2019", new BigDecimal(10000));
+            put("2020", new BigDecimal(12000));
+            put("2021", new BigDecimal(14000));
+        }};
+        staff.setSalary(salary);
+        staff.setSkills(Arrays.asList("java", "python", "node", "kotlin"));
+
+        return staff;
+
+    }
+
+}
--- JsonParser/src/main/java/com/example/JsonParser/JsonParserApplication.java
+++ JsonParser/src/main/java/com/example/JsonParser/JsonParserApplication.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import static com.example.JsonParser.CreateStaff.createStaff;
 
 
+
 public class JsonParserApplication {
 
 	public static void main(String[] args) {
@@ -22,7 +23,7 @@ public class JsonParserApplication {
 
 				ObjectMapper mapper = new ObjectMapper();
 
-				Staff staff = createStaff();
+				Staff staff =createStaff();
 
 				try {
 
